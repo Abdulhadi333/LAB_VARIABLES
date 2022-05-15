f26faa0abbb24bd5c7aa8833061feb6c14f3d2e2
Abdulhadi Ahmasani <103156365+Abdulhadi333@users.noreply.github.com>
2022-05-15 23:04:12 +0300

    add labb1.py

--- labb1.py
+++ labb1.py
@@ -1,1 +1,9 @@
-
+#- Define a variable that has a value of your favourite football team.
+my_var = "Al-hilal"
+#- Define a variable “x“  has an integer value and a variable “y” has a float value.
+my_number:int = 40
+my_number:float = 88888.98765
+#- Assign One Value to Multiple Variables (choose random names & values).
+a, b, c = "Abha"
+#- Assign multiple Values to Multiple Variables (choose random names & values).
+x, y, z = "Abha", "Taiff", "Jeddah"
